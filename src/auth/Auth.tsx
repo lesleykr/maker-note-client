da5f9670ad8b29544ef2e7b5d7d8d9b532fd119d
Lesley Reuter <lesley.reuter@gmail.com>
2021-04-29 19:00:28 -0400

    first commit

---
+++ src/auth/Auth.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Signup from './Signup';
+import Login from './Login';
+
+
+const Auth = (props) => {
+    return(
+       <div>
+                    <Signup updateToken={props.updateToken}/>
+               
+                   <Login updateToken={props.updateToken}/>
+                   </div>
+        
+    )
+}
+
+export default Auth;
