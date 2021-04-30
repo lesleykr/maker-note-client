be46be5e5d6bd8b5585f8c619557f5f31a7d8d88
Lesley Reuter <lesley.reuter@gmail.com>
2021-04-30 11:34:21 -0400

    create endpoint working

--- src/projects/ProjectCreate.tsx
+++ src/projects/ProjectCreate.tsx
@@ -6,21 +6,40 @@ const ProjectCreate = (props) => {
     const [medium, setMedium] = useState('');
     const [totalMaterialCost, setTotalMaterialCost] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3000/projects/create', {
+            method: 'POST',
+            body: JSON.stringify({projects: {projectName: projectName, medium: medium, totalMaterialCost: totalMaterialCost}}),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            })
+        }) .then((res) => res.json())
+        .then((projectsData) => {
+            console.log(projectsData);
+            setProjectName('');
+            setMedium('');
+            setTotalMaterialCost('')
+            props.fetchProjects();
+        })    
+    }
+
     return(
        <>
         <h3>Enter a New Project</h3>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <Label htmlFor="projectName"/>
-                <Input name="projectName" value={projectName}/>
+                <Input name="projectName" value={projectName} onChange={(e) => setProjectName(e.target.value)}/>
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="medium"/>
-                <Input name="medium" value={medium}/>
+                <Input name="medium" value={medium} onChange={(e) => setMedium(e.target.value)}/>
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="totalMaterialCost" />   
-                <Input name="totalMaterialCost" value={totalMaterialCost}/>
+                <Input name="totalMaterialCost" value={totalMaterialCost} onChange={(e) => setTotalMaterialCost(e.target.value)}/>
             </FormGroup>
             <Button type="submit">Save</Button>
         </Form>
---
+++ src/projects/ProjectsTable.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Table, Button} from 'reactstrap';
+
+const ProjectsTable = (props) => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default ProjectsTable
