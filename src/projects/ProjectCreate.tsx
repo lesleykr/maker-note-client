2f9084386d696a63ee4386ccaebd3bee5eb643d4
Lesley Reuter <lesley.reuter@gmail.com>
2021-05-01 18:51:38 -0400

    class ProjectCreate working

--- src/projects/ProjectCreate.tsx
+++ src/projects/ProjectCreate.tsx
@@ -1,50 +1,69 @@
-import React, {useState, useEffect} from 'react';
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const ProjectCreate = (props) => {
-    const [projectName, setProjectName] = useState('');
-    const [medium, setMedium] = useState('');
-    const [totalMaterialCost, setTotalMaterialCost] = useState('');
+interface IProps {
+    fetchProjects: (fetchProjects: string) => void,
+    
+}
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+interface IState {
+    projectName: string,
+    medium: string, 
+    totalMaterialCost: number
+}
+
+export default class ProjectCreate extends Component <IProps, IState>{
+    constructor(props: IProps) {
+        super(props)
+        this.state = {
+            projectName: '',
+            medium: '',
+            totalMaterialCost: 0,            
+        };
+    }
+
+    handleSubmit = (event: any) => {
+        event.preventDefault();
         fetch('http://localhost:3000/projects/create', {
             method: 'POST',
-            body: JSON.stringify({projects: {projectName: projectName, medium: medium, totalMaterialCost: totalMaterialCost}}),
+            body: JSON.stringify({projects: this.state}),
             headers: new Headers({
                 'Content-Type': 'application/json',
-                'Authorization': props.token
+                'Authorization': this.props.token
             })
         }) .then((res) => res.json())
         .then((projectsData) => {
-            console.log(projectsData);
-            setProjectName('');
-            setMedium('');
-            setTotalMaterialCost('')
-            props.fetchProjects();
+            console.log(projectsData);            
+            this.props.fetchProjects();
+            this.setState({
+            projectName: '',
+            medium: '',
+            totalMaterialCost: 0,
+            })
         })    
     }
+
+    render() {
 
     return(
        <>
         <h3>Enter a New Project</h3>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={this.handleSubmit}>
             <FormGroup>
                 <Label htmlFor="projectName"/>
-                <Input name="projectName" value={projectName} onChange={(e) => setProjectName(e.target.value)}/>
+                <Input id="projectName" type="text" name="projectName" value={this.state.projectName} placeholder="Project Name" onChange={(e) => this.setState({projectName: e.target.value})}/>
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="medium"/>
-                <Input name="medium" value={medium} onChange={(e) => setMedium(e.target.value)}/>
+                <Input id="medium" type="text"name="medium" value={this.state.medium} placeholder="Medium" onChange={(e) => this.setState({medium: e.target.value})}/>
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="totalMaterialCost" />   
-                <Input name="totalMaterialCost" value={totalMaterialCost} onChange={(e) => setTotalMaterialCost(e.target.value)}/>
+                <Input id="totalMaterialCost" type="number" name="totalMaterialCost" value={this.state.totalMaterialCost} onChange={(e) => this.setState({totalMaterialCost: e.target.value})}/>
             </FormGroup>
             <Button type="submit">Save</Button>
         </Form>
        </>
     )
 }
-
-export default ProjectCreate;
+}
